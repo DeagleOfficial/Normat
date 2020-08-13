f24805a5b25bd03a67ad8d9ea2666116df24649c
DeagleOfficial <supratikofficial1@gmail.com>
2020-08-14 01:35:40 +0530

    Added JS files

--- conversions.js
+++ conversions.js
@@ -1,2 +1,47 @@
 var decimal = document.getElementById("decimal")
-decimal.value = "0"
+var binary = document.getElementById("binary")
+var octal = document.getElementById("octal")
+var hexadecimal = document.getElementById("hex")
+var convert = document.getElementById("convert")
+var clear = document.getElementById("clear")
+
+clear.onclick = function() {
+    decimal.value = ""
+    binary.value = ""
+    octal.value = ""
+    hexadecimal.value = ""
+}
+
+convert.onclick = function() {
+    if (decimal.value == "" && binary.value == "" && octal.value == "" && hexadecimal.value == "") 
+        alert("Please enter a number")
+    else {
+        if (decimal.value != "") {
+            binary.value = parseInt(decimal.value).toString(2)
+            octal.value = parseInt(decimal.value).toString(8)
+            hexadecimal.value = parseInt(decimal.value).toString(16)
+            return
+        }
+        else if (binary.value != "") {
+            var dec = Number.parseInt(binary.value, 2).toString(10)
+            decimal.value = dec;
+            octal.value = parseInt(decimal.value).toString(8)
+            hexadecimal.value = parseInt(decimal.value).toString(16)
+        }
+
+        else if (octal.value != "") {
+            var dec = Number.parseInt(octal.value, 8).toString(10)
+            decimal.value = dec;
+            binary.value = parseInt(decimal.value).toString(2)
+            hexadecimal.value = parseInt(decimal.value).toString(16)
+        }
+        else if (hexadecimal.value != "") {
+            var dec = Number.parseInt(hexadecimal.value, 16).toString(10)
+            decimal.value = dec;
+            binary.value = parseInt(decimal.value).toString(2)
+            octal.value = parseInt(decimal.value).toString(8)
+        }
+        else return
+    }
+}
+
